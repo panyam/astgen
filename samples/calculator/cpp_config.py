7861cf2daa5fb8c50b83c2855a51ccaf13f0ce0c
Sriram Panyam <sri.panyam@gmail.com>
2014-08-05 00:30:55 -0700

    Updated sample

--- samples/calculator/cpp_config.py
+++ samples/calculator/cpp_config.py
@@ -1,4 +1,10 @@
 
+LAYOUT_BACKEND = "astgen.layouts.TwoFilesPerNodeLayout"
+
+FWDDEFS_OUTPUT = "CalculatorASTFwds.h"
+PUBLIC_OUTPUT = "CalculatorAST.h"
+ENUMS_OUTPUT = "CalculatorASTEnums.h"
+
 """
 Name of the namespace underwhich all the node definitions will be generated.
 """
@@ -8,13 +14,15 @@ NAMESPACE = "Calculator"
 Templates for the header and implementation respectively
 """
 HEADER_OUTPUT = "CalculatorAST.h"
-IMPLEMENTATION_OUTPUT = "CalculatorAST.cpp"
+IMPL_OUTPUT = "CalculatorAST.cpp"
 
 INITIAL_HEADERS = """
 #include <list>
 #include <map>
 #include <string>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -26,7 +34,9 @@ The preamble that gets included *before* any code is generated.
 NAMESPACE_PREAMBLE = """
 """
 
-IMPLEMENTATION_PREAMBLE = """
+IMPL_PREAMBLE = """
 #include "%s"
 """ % HEADER_OUTPUT
 
+def headerFilenameForNode(node): return node.nodeName() + ".h"
+def implFilenameForNode(node): return node.nodeName() + ".cpp"
